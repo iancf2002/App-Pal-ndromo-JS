e9dde4eac0c022716250fbe0a3f4f26f1630743d
iancf2002 <93164795+iancf2002@users.noreply.github.com>
2024-08-16 14:23:08 -0300

    Update index.js
    adicionado a explicação das funções do mini App

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const input = document.getElementById("input")
 
+//função que reverte a palavra, o SPLIT separa a palavra em letras o REVERSE inverte a ordem delas e o JOIN as junta novamente (string)
 function reverterString(str) {
     return str.split("").reverse().join("")
     
     
 }
-
+//função que faz a checagem se a palavra escrita pelo usuário é de fato um palíndromo ou não. imput.value= "" é para limpar a caixa de texto após a checagem
 function checar() {
     const value = input.value
     const reverse = reverterString(value)
